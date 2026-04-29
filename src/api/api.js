91c851ef9badd3225ecbe31bdea6908e065daefe
Sajfarsaju <sajfarmmuhammad@gmail.com>
2026-04-29 22:04:18 +0530

    Updated api url

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const AXIOS_API = axios.create(
     {
-        baseURL: "http://localhost:4000"
+        // baseURL: "http://localhost:4000"
+        baseURL: "https://shopui-server.onrender.com"
     }
 )
 export default AXIOS_API
